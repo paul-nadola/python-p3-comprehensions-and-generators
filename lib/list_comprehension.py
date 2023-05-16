c275c27ebc42d478417b693f0baa3d7c25dad9fa
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-05-16 14:28:49 +0300

    Done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [i for i in num_list if i % 2 == 0]
+    # evens = [ i % 2 == 0 for i in num_list] returns booleans
+    # for i in num_list:
+    #     return [i if i % 2 == 0 else []]
+    # for i in (num_list):
+    #     evens = []
+    #     if i % 2 == 0:
+    #         evens.append(i)
+    #     return evens
+
+
+print(return_evens([0, 1, 3, 5, 7, 8, 9]))
+
 
 def make_exclamation(sentence_list):
-    pass
+    return [i + "!" for i in sentence_list]
